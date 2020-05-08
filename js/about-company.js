3af5907c218fdb09e0aa2e5d546adc6e40cb61af
Kydryavcev <kudryavgev.ilay.2003@gmail.com>
2020-05-08 13:50:55 +0300

    finished works without sidebar

--- js/about-company.js
+++ js/about-company.js
@@ -1,22 +1,18 @@
 let MainElem = document.getElementById("EquipmentBlock");
 
-let ElemetsEquipment = Array.from(MainElem.children);
-
-let length = ElemetsEquipment.length
-
 let back = document.getElementById("back");
 let next = document.getElementById("next");
 
 back.onclick = function(event) {
     first = MainElem.firstElementChild;
     last = MainElem.lastElementChild;
-    MainElem.firstElementChild.remove()
-    MainElem.insertAdjacentHTML('beforeend', first.outerHTML);
-}
+    MainElem.lastElementChild.remove()
+    MainElem.insertAdjacentHTML('afterbegin', last.outerHTML);
+} 
 
 next.onclick = function(event) {
     first = MainElem.firstElementChild;
     last = MainElem.lastElementChild;
-    MainElem.lastElementChild.remove()
-    MainElem.insertAdjacentHTML('afterbegin', last.outerHTML);
+    MainElem.firstElementChild.remove()
+    MainElem.insertAdjacentHTML('beforeend', first.outerHTML);
 }
