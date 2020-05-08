2100a86866617a242daa5679b5ea63ba1b2ff2ed
Kydryavcev <kudryavgev.ilay.2003@gmail.com>
2020-05-08 11:45:24 +0300

    index page

--- js/index.js
+++ js/index.js
@@ -40,3 +40,49 @@ list2.onclick = function (event) {
         elem.nextElementSibling.style.display = "block";
     } 
 }
+
+let MainElem = document.getElementById("SponsorsBlock");
+
+let back = document.getElementById("back-sponsors");
+let next = document.getElementById("next-sponsors");
+
+back.onclick = function(event) {
+    first = MainElem.firstElementChild;
+    last = MainElem.lastElementChild;
+    MainElem.lastElementChild.remove()
+    MainElem.insertAdjacentHTML('afterbegin', last.outerHTML);
+} 
+
+next.onclick = function(event) {
+    first = MainElem.firstElementChild;
+    last = MainElem.lastElementChild;
+    MainElem.firstElementChild.remove()
+    MainElem.insertAdjacentHTML('beforeend', first.outerHTML);
+}
+
+let ProductsBlock  = document.getElementById("ProductsBlock");
+
+let ChevronProduct = document.getElementById("ListProductChevron");
+let ChevronChild   = Array.from(ChevronProduct.children);
+
+let ActiveChevron;
+
+for (let i = 0; i < ChevronChild.length; i++) {
+    if (ChevronChild[i].classList.contains("active")) {
+        ActiveChevron = ChevronChild[i];
+        break;
+    }   
+}
+
+ChevronProduct.onclick = function (event) {
+    elem = event.target||event.srcElement;
+    ActiveChevron.classList.remove("active")
+    elem.classList.toggle("active");
+    number = elem.innerHTML + 1;
+    ActiveChevron = ChevronChild[elem.innerHTML];
+
+    // for (let index = 0; index < array.length; index++) {
+    //     const element = array[index];
+        
+    // }
+}
